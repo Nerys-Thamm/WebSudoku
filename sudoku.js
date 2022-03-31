038e8aca083c1552bb04139111450fdcdcceab00
Nerys-Thamm <nerysthamm@mgail.com>
2022-03-31 17:15:39 +1300

    Commented

--- sudoku.js
+++ sudoku.js
@@ -1,18 +1,22 @@
-var currCellID = "0-0"
-const rowlen = 9;
-const rownum = 9;
-const maxSolverIterations = 100;
+var currCellID = "0-0" // the id of the currently selected cell
+const rowlen = 9; // the length of a row in the sudoku grid
+const rownum = 9; // the number of rows in the sudoku grid
+const maxSolverIterations = 100; // the maximum number of times the solver will try to solve the sudoku
 
+// returns the indices of the cell with the given id
 function getSingleIndex(i, j) {
     return i * rowlen + j;
 }
 
+// clamp the given value to the range [min, max]
 function clamp(val, min, max) {
     return Math.min(Math.max(val, min), max);
 }
 
+
+// Class representing a sudoku grid
 class Sudoku {
-    constructor() {
+    constructor() {//Constructor
         this.cells = new Array(rowlen * rownum);
         this.unmutableCells = new Array(rowlen * rownum);
         for (let i = 0; i < rownum; i++) {
@@ -22,7 +26,7 @@ class Sudoku {
             }
         }
     }
-    static copy(sudoku) {
+    static copy(sudoku) {//Copy Constructor
         let newSudoku = new Sudoku();
         for (let i = 0; i < rownum; i++) {
             for (let j = 0; j < rowlen; j++) {
@@ -51,7 +55,7 @@ class Sudoku {
         let [i, j] = cellID.split("-");
         this.setCell(parseInt(i), parseInt(j), clamp(value, 1, 9));
     }
-    updateHTML() {
+    updateHTML() { //Update the HTML of the page
         for (let i = 0; i < rownum; i++) {
             for (let j = 0; j < rowlen; j++) {
                 let cell = this.getCell(i, j);
@@ -62,7 +66,7 @@ class Sudoku {
     }
 
     
-
+    //Generate a sudoku with the provided number of clues
     static generateNewSudoku(clueNum) {
         let sudoku = new Sudoku();
         let clueCount = 0;
@@ -99,14 +103,14 @@ class Sudoku {
         }
         return true;
     }
-    checkValidRow(i) {
+    checkValidRow(i) { //Check if the row is valid
         let row = new Array(9);
         for (let j = 0; j < rowlen; j++) {
             row[j] = this.getCell(i, j);
         }
         return Sudoku.checkValid(row);
     }
-    checkValidColumn(j) {
+    checkValidColumn(j) { //Check if the column is valid
         let column = new Array(9);
         for (let i = 0; i < rownum; i++) {
             column[i] = this.getCell(i, j);
@@ -114,7 +118,7 @@ class Sudoku {
         return Sudoku.checkValid(column);
     }
     
-    checkValidSubgrid(i, j) {
+    checkValidSubgrid(i, j) { //Check if the subgrid is valid
         let subgrid = new Array(9);
         let subgridIndex = 0;
         for (let k = 0; k < 3; k++) {
@@ -125,7 +129,7 @@ class Sudoku {
         }
         return Sudoku.checkValid(subgrid);
     }
-    static checkValidSudoku(sudoku) {
+    static checkValidSudoku(sudoku) { //Check if the sudoku is valid
         for (let i = 0; i < rownum; i++) {
             if (!sudoku.checkValidRow(i)) {
                 return false;
@@ -154,46 +158,46 @@ class Sudoku {
                 }
             }
         }
-        return complete && Sudoku.checkValidSudoku(sudoku);
+        return complete && Sudoku.checkValidSudoku(sudoku); //Check if the sudoku is complete and valid
     }
-    setValidPlacementCSS() {
-        for (let i = 0; i < rownum; i++) {
-            for (let j = 0; j < rowlen; j++) {
-                let cellID = i + "-" + j;
-                let cell = this.getCell(i, j);
-                if (cell == 0) {
-                    document.getElementById(cellID).style.backgroundColor = "OldLace";
-                } else if(!this.getCellMutableFromID(cellID)) {
-                    document.getElementById(cellID).style.backgroundColor = "PaleGoldenRod";
-                } else if(this.checkValidRow(i) && this.checkValidColumn(j) && this.checkValidSubgrid(i - (i % 3), j - (j % 3))) {
-                    document.getElementById(cellID).style.backgroundColor = "darkseagreen";
-                } else {
-                    document.getElementById(cellID).style.backgroundColor = "red";
+    setValidPlacementCSS() { //Set the CSS of the cells to indicate valid placement
+        for (let i = 0; i < rownum; i++) { //For each row
+            for (let j = 0; j < rowlen; j++) { //For each cell
+                let cellID = i + "-" + j; //Get the ID of the cell
+                let cell = this.getCell(i, j); //Get the value of the cell
+                if (cell == 0) { //If the cell is empty
+                    document.getElementById(cellID).style.backgroundColor = "OldLace"; //Set the background color to OldLace
+                } else if(!this.getCellMutableFromID(cellID)) { //If the cell is not mutable
+                    document.getElementById(cellID).style.backgroundColor = "PaleGoldenRod"; //Set the background color to PaleGoldenRod
+                } else if(this.checkValidRow(i) && this.checkValidColumn(j) && this.checkValidSubgrid(i - (i % 3), j - (j % 3))) { //If the cell is valid
+                    document.getElementById(cellID).style.backgroundColor = "darkseagreen"; //Set the background color to darkseagreen
+                } else { //If the cell is invalid
+                    document.getElementById(cellID).style.backgroundColor = "red"; //Set the background color to red
                 }
             }
         }
-        if(currCellID != null && currCellID != undefined && this.getCellMutableFromID(currCellID)) document.getElementById(currCellID).style.backgroundColor = "orange";
-        else if(currCellID != null && currCellID != undefined) document.getElementById(currCellID).style.backgroundColor = "PeachPuff";
+        if(currCellID != null && currCellID != undefined && this.getCellMutableFromID(currCellID)) document.getElementById(currCellID).style.backgroundColor = "orange"; //Set the background color of the current cell to orange
+        else if(currCellID != null && currCellID != undefined) document.getElementById(currCellID).style.backgroundColor = "PeachPuff"; //Set the background color of the current cell to PeachPuff
     }
 
-    trimToClues(clueNum) {
-        var clueCount = 9;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                clueCount = 9
-                while (clueCount > clueNum) {
-                    let randI = Math.floor(Math.random() * 3);
-                    let randJ = Math.floor(Math.random() * 3);
-                    if (this.getCell((i*3) + randI, (j*3) + randJ) != 0) {
-                        this.setCell((i*3) + randI, (j*3) + randJ, 0);
-                        this.unmutableCells[getSingleIndex((i*3) + randI, (j*3) + randJ)] = true;
-                        clueCount--;
+    trimToClues(clueNum) { //Trim the sudoku to the number of clues
+        var clueCount = 9; //Number of clues
+        for (let i = 0; i < 3; i++) { //Check each subgrid
+            for (let j = 0; j < 3; j++) { //Check each subgrid
+                clueCount = 9 
+                while (clueCount > clueNum) { //While there are more clues than the number we want
+                    let randI = Math.floor(Math.random() * 3); //Randomly choose a row
+                    let randJ = Math.floor(Math.random() * 3); //Randomly choose a column
+                    if (this.getCell((i*3) + randI, (j*3) + randJ) != 0) { //If the cell is not empty
+                        this.setCell((i*3) + randI, (j*3) + randJ, 0); //Set the cell to empty
+                        this.unmutableCells[getSingleIndex((i*3) + randI, (j*3) + randJ)] = true; //Set the cell to mutable
+                        clueCount--; //Decrement the clue count
                     }
                 }
-                for (let k = 0; k < 3; k++) {
-                    for (let l = 0; l < 3; l++) {
-                        if (this.getCell((i*3) + k, (j*3) + l) != 0) {
-                            this.unmutableCells[getSingleIndex((i*3) + k, (j*3) + l)] = false;
+                for (let k = 0; k < 3; k++) { //Check each row
+                    for (let l = 0; l < 3; l++) { //Check each column
+                        if (this.getCell((i*3) + k, (j*3) + l) != 0) { //If the cell is not empty
+                            this.unmutableCells[getSingleIndex((i*3) + k, (j*3) + l)] = false; //Set the cell to unmutable
                         }
                     }
                 }
@@ -204,25 +208,25 @@ class Sudoku {
 
 
 var timeOut = 0
-class Solver {
+class Solver {//Solves the sudoku
     static solve(sudoku, iterations) {
-        if(iterations == 0) timeOut = 0
-        else timeOut += 1
-        if(timeOut > 2000) return false
-        if(iterations > maxSolverIterations) return false;
-        for (let i = 0; i < rownum; i++) {
-            for (let j = 0; j < rowlen; j++) {
-                if (sudoku.getCell(i, j) == 0) {
-                    for (let n = 1; n <= 9; n++) {
-                        sudoku.setCell(i, j, n);
-                        if (sudoku.checkValidRow(i) && sudoku.checkValidColumn(j) && sudoku.checkValidSubgrid(i - (i % 3), j - (j % 3))) {
-                            if (Solver.solve(sudoku, iterations++)) {
-                                return true;
-                            }else if(iterations > maxSolverIterations) return false;
-                        }else if(iterations > maxSolverIterations) return false;
+        if(iterations == 0) timeOut = 0 //Reset timeout
+        else timeOut += 1 //increment timeOut
+        if(timeOut > 2000) return false //If the solver has been stuck for too long, return false
+        if(iterations > maxSolverIterations) return false; //If the solver has been stuck for too long, return false
+        for (let i = 0; i < rownum; i++) { //check all rows
+            for (let j = 0; j < rowlen; j++) { //check all columns
+                if (sudoku.getCell(i, j) == 0) { //if cell is empty
+                    for (let n = 1; n <= 9; n++) { //check all numbers
+                        sudoku.setCell(i, j, n); //try setting the number
+                        if (sudoku.checkValidRow(i) && sudoku.checkValidColumn(j) && sudoku.checkValidSubgrid(i - (i % 3), j - (j % 3))) { //if valid
+                            if (Solver.solve(sudoku, iterations++)) { //recurse
+                                return true; //if solved
+                            }else if(iterations > maxSolverIterations) return false; //if max iterations reached
+                        }else if(iterations > maxSolverIterations) return false; //if max iterations reached
                     }
-                    sudoku.setCell(i, j, 0);
-                    return false;
+                    sudoku.setCell(i, j, 0); //if no valid numbers, set cell back to 0
+                    return false; //if no valid numbers, return false
                 }
             }
         }
@@ -235,83 +239,83 @@ class Solver {
 
 
 
-function getDifficultyNum(difficulty) {
+function getDifficultyNum(difficulty) { //returns the difficulty number for the difficulty
     if (difficulty == "Easy") return 4;
     else if (difficulty == "Medium") return 3;
     else if (difficulty == "Hard") return 2;
     else return 1;
 }
 
-function generateSudoku(difficulty) {
+function generateSudoku(difficulty) { //generates a sudoku with the difficulty
     let numClues = getDifficultyNum(difficulty);
     var newSudoku = Sudoku.generateNewSudoku(1);
-    while(!Solver.solve(newSudoku, 0))
+    while(!Solver.solve(newSudoku, 0)) //solves the sudoku until it is solvable
     {
         newSudoku = Sudoku.generateNewSudoku(1)
     }
-    newSudoku.trimToClues(numClues);
+    newSudoku.trimToClues(numClues); //trims the sudoku to the number of clues
     return newSudoku;
 }
 
-var difficulty = "Easy";
-var sudoku = generateSudoku(difficulty);
+var difficulty = "Easy"; //default difficulty
+var sudoku = generateSudoku(difficulty); //generates a sudoku with the difficulty
 
 
 
-generateSudoku(difficulty);
+generateSudoku(difficulty); //generates a sudoku with the difficulty
 
 
-$(document).ready(function () {
-    $("td").hover(function () {
-        let cellID = $(this).attr("id");
-        currCellID = cellID;
-        let cell = sudoku.getCellFromID(cellID);
-        sudoku.setValidPlacementCSS();
+$(document).ready(function () { //Called when the document is ready
+    $("td").hover(function () { //Called when the mouse hovers over a cell
+        let cellID = $(this).attr("id"); //Gets the ID of the cell
+        currCellID = cellID; //Sets the current cell ID
+        let cell = sudoku.getCellFromID(cellID); //Gets the cell from the ID
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
     },
-        function () {
-        currCellID = null;
-        sudoku.setValidPlacementCSS();
+        function () { //Called when the mouse leaves a cell
+        currCellID = null; //Sets the current cell ID to null
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
         });
-    $("td").click(function () {
-        let cellID = $(this).attr("id");
-        let cell = sudoku.getCellFromID(cellID);
-        sudoku.setCellFromID(cellID, cell + 1);
+    $("td").click(function () { //Called when the cell is clicked
+        let cellID = $(this).attr("id"); //Gets the ID of the cell
+        let cell = sudoku.getCellFromID(cellID); //Gets the cell from the ID
+        sudoku.setCellFromID(cellID, cell + 1); //Sets the cell to the next number
         
-        sudoku.updateHTML();
-        sudoku.setValidPlacementCSS();
+        sudoku.updateHTML(); //Updates the HTML
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
     });
-    $(window).keypress(function (e) {
-        if (e.keyCode == 8) {
-            if(sudoku.getCellMutableFromID(currCellID)) sudoku.setCellFromID(currCellID, 0);
-        } else {
-            if(sudoku.getCellMutableFromID(currCellID)) sudoku.setCellFromID(currCellID, e.keyCode - 48);
+    $(window).keypress(function (e) { //Called when a key is pressed
+        if (e.keyCode == 8) { //If the key is backspace
+            if(sudoku.getCellMutableFromID(currCellID)) sudoku.setCellFromID(currCellID, 0); //Sets the cell to 0
+        } else { //If the key is not backspace
+            if(sudoku.getCellMutableFromID(currCellID)) sudoku.setCellFromID(currCellID, e.keyCode - 48); //Sets the cell to the number
         }
-        sudoku.updateHTML();
-        sudoku.setValidPlacementCSS();
-        if(Sudoku.checkCompleteSudoku(sudoku)){
-            alert("You Win!");
+        sudoku.updateHTML(); //Updates the HTML
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
+        if(Sudoku.checkCompleteSudoku(sudoku)){ //Checks if the sudoku is complete
+            alert("You Win!"); //Alerts the user they won
         }
     });
-    $("#new").click(function () {
-        sudoku = generateSudoku(difficulty);
-        sudoku.updateHTML();
-        sudoku.setValidPlacementCSS();
+    $("#new").click(function () { //Called when the new button is clicked
+        sudoku = generateSudoku(difficulty); //Generates a new sudoku
+        sudoku.updateHTML(); //Updates the HTML
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
     });
-    $("#solve").click(function () {
-        timeOut = 0
-        Solver.solve(sudoku);
-        sudoku.updateHTML();
-        sudoku.setValidPlacementCSS();
+    $("#solve").click(function () { //Called when the solve button is clicked
+        timeOut = 0 //Resets the timeout
+        Solver.solve(sudoku); //Solves the sudoku
+        sudoku.updateHTML(); //Updates the HTML
+        sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
     });
-    $("#difficulty").click(function() {
-        let currDifficulty = document.getElementById("difficulty").innerHTML;
-        if(currDifficulty == "Difficulty: Easy") difficulty = "Medium";
-        else if(currDifficulty == "Difficulty: Medium") difficulty = "Hard";
-        else if(currDifficulty == "Difficulty: Hard") difficulty = "Easy";
-        document.getElementById("difficulty").innerHTML = "Difficulty: " + difficulty;
+    $("#difficulty").click(function() { //Called when the difficulty button is clicked
+        let currDifficulty = document.getElementById("difficulty").innerHTML; //Gets the current difficulty
+        if(currDifficulty == "Difficulty: Easy") difficulty = "Medium"; //Sets the difficulty to medium
+        else if(currDifficulty == "Difficulty: Medium") difficulty = "Hard"; //Sets the difficulty to hard
+        else if(currDifficulty == "Difficulty: Hard") difficulty = "Easy"; //Sets the difficulty to easy
+        document.getElementById("difficulty").innerHTML = "Difficulty: " + difficulty; //Updates the difficulty
     })
-    sudoku.updateHTML();
-    sudoku.setValidPlacementCSS();
+    sudoku.updateHTML(); //Updates the HTML
+    sudoku.setValidPlacementCSS(); //Updates the valid placement CSS
 });
 
 
